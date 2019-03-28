f4b31f0f812945208b19487c3a9359373c474854
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-28 17:03:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,4 +12,11 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    
+    if age > 10 
+      puts "You are too old for this."
+    else
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
+      
